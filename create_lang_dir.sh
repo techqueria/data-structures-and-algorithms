e7e590948bc9c77257b05732682b90ea791a1616
Noe Lomeli <nolomeli@csumb.edu>
2018-11-03 11:59:33 -0700

    create chapter directories 2nd attempt

---
+++ create_lang_dir.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+if [ -d "Big O" ]
+then echo "Directory exists"
+else
+`mkdir "Big O"`
+echo "Big O Directory created"
+fi
+
+if [ -d "Ch 01. Arrays and Strings" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 01. Arrays and Strings"`
+echo "Ch 01 Directory created"
+fi
+
+if [ -d "Ch 02. Linked Lists" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 02. Linked Lists"`
+echo "Ch 02 Directory created"
+fi
+
+if [ -d "Ch 03. Stacks and Queues" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 03. Stacks and Queues"`
+echo "Ch 03 Directory created"
+fi
+
+if [ -d "Ch 04. Trees and Graphs" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 04. Trees and Graphs"`
+echo "Ch 04 Directory created"
+fi
+
+if [ -d "Ch 05. Bit Manipulation" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 05. Bit Manipulation"`
+echo "Ch 05 Directory created"
+fi
+
+if [ -d "Ch 06. Math and Logic Puzzles" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 06. Math and Logic Puzzles"`
+echo "Ch 06 Directory created"
+fi
+
+if [ -d "Ch 07. Object-Oriented Design" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 07. Object-Oriented Design"`
+echo "Ch 07 Directory created"
+fi
+
+if [ -d "Ch 08. Recursion and Dynamic Programming" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 08. Recursion and Dynamic Programming"`
+echo "Ch 08 Directory created"
+fi
+
+if [ -d "Ch 09. Scalability and Memory Limits" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 09. Scalability and Memory Limits"`
+echo "Ch 09 Directory created"
+fi
+
+if [ -d "Ch 10. Sorting and Searching" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 10. Sorting and Searching"`
+echo "Ch 10 Directory created"
+fi
+
+if [ -d "Ch 13. Java" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 13. Java"`
+echo "Ch 13 Directory created"
+fi
+
+if [ -d "Ch 15. Threads and Locks" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 15. Threads and Locks"`
+echo "Ch 15 Directory created"
+fi
+
+if [ -d "Ch 16. Moderate" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 16. Moderate"`
+echo "Ch 16 Directory created"
+fi
+
+if [ -d "Ch 17. Hard" ]
+then echo "Directory exists"
+else
+`mkdir "Ch 17. Hard"`
+echo "Ch 17 Directory created"
+fi
