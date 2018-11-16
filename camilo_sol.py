8e6d211b4237a79df1504611147d6dc6e02e2f8b
Camilo <camilosalazar45@gmail.com>
2018-11-16 15:27:28 -0500

    Add files via upload

---
+++ camilo_sol.py
@@ -0,0 +1,16 @@
+# Implement an algorithm to determine if a string has all unique charactersself.
+# What if you cannot use additional data structures.
+
+
+def isUnique(string_):
+    for i in range(len(string_)):
+        i = 0
+        for j in range(1,len(string_)):
+            if string_[i] == string_[j]:
+                return False
+    return True
+
+print(isUnique("GeeksforGeeks"))
+#This soluation is O(n^2)
+
+#let's see if we can go faster
