f4d85ca5e5b2e6d5fd3ebb62f0dc1b87f1ea7ed4
Camilo Salazar <camilosalazar45@gmail.com>
2018-11-17 20:17:35 -0500

    solutions

--- Python/chapter01/1.1 - Is Unique/camilo_sol.py
+++ Python/chapter01/1.1 - Is Unique/camilo_sol.py
@@ -1,5 +1,7 @@
-# Implement an algorithm to determine if a string has all unique charactersself.
-# What if you cannot use additional data structures.
+"""
+Is Unique: Implement an algorithm to determine if a string has all unique
+characters. What if you cannot use additional data structures?
+"""
 
 
 def isUnique(string_):
