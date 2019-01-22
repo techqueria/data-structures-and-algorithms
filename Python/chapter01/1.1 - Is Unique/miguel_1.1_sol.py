22e95b528c66100384f72a68422f5d89b50cd2d8
miguelhx <mhernandez401405@gmail.com>
2019-01-21 19:18:27 -0800

    empty str returns true, using types in function signature

--- Python/chapter01/1.1 - Is Unique/miguel_1.1_sol.py
+++ Python/chapter01/1.1 - Is Unique/miguel_1.1_sol.py
@@ -12,25 +12,13 @@ import unittest
 from typing import Callable
 
 
-def _validate_input_str(input_str: str) -> None:
-    """
-    Does some input checking for the is_unique function and its variants
-    Raises exceptions if input_str is empty or if input_str is not of type string
-    :param input_str: a string that we are checking to make
-    :return:
-    """
-    if input_str == "":
-        raise ValueError("empty input string")
-
-
-def is_unique(input_str):
+def is_unique(input_str: str) -> bool:
     """
     Determines if input_str has all unique characters.
     For all characters in the input_str with n >= 1, where n is the number of characters in input_str,
     there must be no duplicate characters.
     In the worst case, the run time of this function will be O(n)
     Space complexity will be O(n)
-    An exception will be raised to handle faulty input.
     Given:		Expect:
     tacos		True
     swag		True
@@ -38,10 +26,10 @@ def is_unique(input_str):
     california	False
     orbit       True
     e       True
+    ""      True
     :param input_str: the string we want to check characters of
     :return: returns True if input_str has all unique characters, False otherwise
     """
-    _validate_input_str(input_str)
     chars_seen = set()
     for c in input_str:
         if c in chars_seen:
@@ -50,7 +38,7 @@ def is_unique(input_str):
     return True
 
 
-def is_unique_no_additional_data_structures(input_str):
+def is_unique_no_additional_data_structures(input_str: str) -> bool:
     """
     Variant of is_unique.  Uses no additional data structures (besides the variables from the iterator)
     However, the drawback is that the runtime is O((n*n-1)/2) = O(n^2), where n is the number of characters
@@ -62,10 +50,10 @@ def is_unique_no_additional_data_structures(input_str):
     california	False
     orbit       True
     e       True
+    ""      True
     :param input_str: the string we want to check characters of
     :return: returns True if input_str has all unique characters, False otherwise
     """
-    _validate_input_str(input_str)
     for i, c in enumerate(input_str):
         if c in input_str[i+1:]:
             return False
@@ -76,12 +64,10 @@ class TestIsUniqueFunction(unittest.TestCase):
     def _run_tests(self, f: Callable[[str], None]) -> None:
         for case in ["techqueria", "bobby", "california"]:
             self.assertFalse(f(case), msg=case)
-        for case in ["tacos", "swag", "orbit", "e"]:
+        for case in ["tacos", "swag", "orbit", "e", ""]:
             self.assertTrue(f(case), msg=case)
         with self.assertRaises(TypeError):
             f(8)
-        with self.assertRaises(ValueError):
-            f("")
 
     def test_is_unique(self):
         self._run_tests(is_unique)
