3e3db7807f79f35d1e6a0aa20f498840628e110e
miguelhx <mhernandez401405@gmail.com>
2019-01-21 16:32:07 -0800

    use unittest mod, address comments from PR review

--- Python/chapter01/1.1 - Is Unique/miguel_1.1_sol.py
+++ Python/chapter01/1.1 - Is Unique/miguel_1.1_sol.py
@@ -3,6 +3,10 @@ Python version 3.7.0
 1.1
 Implement an algorithm to determine if a string has all unique characters.
 What if you cannot use additional data structures?
+What if the input string is empty?
+I assume that the user will input a non-empty string
+If the string is empty, then raise a ValueError exception
+If the input is NOT a string, then raise a TypeError exception
 
 Given:		Expect:
 tacos		true
@@ -12,49 +16,61 @@ california	false
 orbit		true
 e		    true
 """
+import unittest
+
+
+def check_is_unique_input(input_str):
+    if not isinstance(input_str, str):
+        raise TypeError("expected string as input")
+    if input_str == "":
+        raise ValueError("empty input string")
 
 
 def is_unique(input_str):
-    freqs = {}
+    check_is_unique_input(input_str)
+    chars_seen = set()
     for c in input_str:
-        if c not in freqs:
-            freqs[c] = 1
-        else:
+        if c in chars_seen:
             return False
+        chars_seen.add(c)
     return True
 
 
 def is_unique_no_additional_data_structures(input_str):
-    for i in range(0, len(input_str)):
-        for j in range(i+1, len(input_str)):
-            if input_str[i] == input_str[j]:
-                return False
+    check_is_unique_input(input_str)
+    for i, c in enumerate(input_str):
+        if c in input_str[i+1:]:
+            return False
     return True
 
 
-def main():
-    test1 = "tacos"
-    test2 = "swag"
-    test3 = "bobby"
-    test4 = "california"
-    test5 = "orbit"
-    test6 = "e"
+class TestIsUniqueFunction(unittest.TestCase):
+    def test_is_unique(self):
+        self.assertFalse(is_unique("techqueria"))
+        self.assertTrue(is_unique("tacos"))
+        self.assertTrue(is_unique("swag"))
+        self.assertFalse(is_unique("bobby"))
+        self.assertFalse(is_unique("california"))
+        self.assertTrue(is_unique("orbit"))
+        self.assertTrue(is_unique("e"))
+        with self.assertRaises(TypeError):
+            is_unique(8)
+        with self.assertRaises(ValueError):
+            is_unique("")
 
-    # run tests, want to get all 'True' in the console output
-    print(is_unique(test1) == True)
-    print(is_unique(test2) == True)
-    print(is_unique(test3) == False)
-    print(is_unique(test4) == False)
-    print(is_unique(test5) == True)
-    print(is_unique(test6) == True)
-
-    print(is_unique_no_additional_data_structures(test1) == True)
-    print(is_unique_no_additional_data_structures(test2) == True)
-    print(is_unique_no_additional_data_structures(test3) == False)
-    print(is_unique_no_additional_data_structures(test4) == False)
-    print(is_unique_no_additional_data_structures(test5) == True)
-    print(is_unique_no_additional_data_structures(test6) == True)
+    def test_is_unique_no_additional_data_structures(self):
+        self.assertFalse(is_unique_no_additional_data_structures("techqueria"))
+        self.assertTrue(is_unique_no_additional_data_structures("tacos"))
+        self.assertTrue(is_unique_no_additional_data_structures("swag"))
+        self.assertFalse(is_unique_no_additional_data_structures("bobby"))
+        self.assertFalse(is_unique_no_additional_data_structures("california"))
+        self.assertTrue(is_unique_no_additional_data_structures("orbit"))
+        self.assertTrue(is_unique_no_additional_data_structures("e"))
+        with self.assertRaises(TypeError):
+            is_unique_no_additional_data_structures(8)
+        with self.assertRaises(ValueError):
+            is_unique_no_additional_data_structures("")
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
