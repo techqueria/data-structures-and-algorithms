3d74de4b952e5eff356db60be40966442642ff5a
Ricardo <rroque98@gmail.com>
2019-01-26 09:48:34 -0800

    Refactor 1.4 PalinPerm with ES6 Map solution

--- JavaScript/chapter01/1.4 - PalinPerm/rroque98_sol.js
+++ JavaScript/chapter01/1.4 - PalinPerm/rroque98_sol.js
@@ -1,20 +1,17 @@
 // Given a string, write a function to check if it is a permutation of a palindrome
 
 const isPalindromePermutation = (str) => {
-  const strNoSpaces = str.split(' ').join('');
-  const obj = {};
+  const strNoSpaces = str.replace(/ /g, '');
   var oddCount = 0;
+  const frequencies = new Map();
   for (let char of strNoSpaces) {
-    if (obj[char] !== undefined) {
-      obj[char]++;
-    } else {
-      obj[char] = 1;
-    }
+    frequencies.set(char, 1 + (frequencies.get(char) || 0));
   }
-  for (let key in obj) {
-    if (obj[key] % 2) {
-      oddCount++;
+  for (let frequency of frequencies.values()) {
+    if (frequency % 2 === 0) {
+     continue;
     }
+    oddCount++;
     if (oddCount > 1) {
       return false;
     }
