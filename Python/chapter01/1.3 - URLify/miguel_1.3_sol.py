93e2a71597742de7eb7c6c84ed58f22e1e1a31dc
miguelhx <mhernandez401405@gmail.com>
2019-01-25 20:30:29 -0800

    update with no true length function, new slightly modded tests, comments explaining why no true length

--- Python/chapter01/1.3 - URLify/miguel_1.3_sol.py
+++ Python/chapter01/1.3 - URLify/miguel_1.3_sol.py
@@ -1,9 +1,12 @@
 """
 Python version 3.7.0
 1.3 - URLify
-Write a method to replace all spaces in a string with "%20".  You may assume that the string has sufficient
-space at the end to hold the additional characters, and that you are given the "true" length of the string.
+Write a method to replace all spaces in a string with "%20".
+The following only applies to C/C++: "You may assume that the string has sufficient
+space at the end to hold the additional characters, and that you are given the "true" length of the string"
 (Note: If implementing in Java, please use a character array so that you can perform this operation in place)
+Since we are using python, no need to use true length.  I will have two functions, one with
+true_length and the other without.
 """
 import unittest
 from typing import Callable
@@ -32,8 +35,29 @@ def urlify(s: str, true_length: int) -> str:
     return ''.join(output)
 
 
+def urlify_no_true_length(s: str) -> str:
+    """
+    This is the version of urlify without the true_length argument.
+    Given a string and it's "true" length, this function will return a new string
+    that replaces all of the spaces of the input string with '%20'.
+    Note that this will return a different result than the other urlify
+    if the other urlify's true length truncates the string with chars left over.
+    Precondition(s):
+    - length of s <= true_length
+    :param s: the original string to 'urlify'
+    :return: string with each space from s replaced with '%20'
+    """
+    output = []
+    for c in s:
+        if c == ' ':
+            output.append("%20")
+            continue
+        output.append(c)
+    return ''.join(output)
+
+
 class TestUrlifyFunction(unittest.TestCase):
-    def _run_tests(self, f: Callable[[str, int], str]) -> None:
+    def _run_tests(self, f1: Callable[[str, int], str], f2: Callable[[str], str]) -> None:
         cases = [
             (("Mr John Smith     ", 13), "Mr%20John%20Smith"),
             (("Miguel Hernandez", 16), "Miguel%20Hernandez"),
@@ -43,11 +67,22 @@ class TestUrlifyFunction(unittest.TestCase):
             (("ihavenospaces", 13), "ihavenospaces"),
             (("nospacesIgnoreme", 8), "nospaces")
         ]
+        cases_no_true_length = [
+            ("Mr John Smith", "Mr%20John%20Smith"),
+            ("Miguel Hernandez", "Miguel%20Hernandez"),
+            (" Techqueria ", "%20Techqueria%20"),
+            ("a b c d e f g h", "a%20b%20c%20d%20e%20f%20g%20h"),
+            ("a b c d e f g h ignore this", "a%20b%20c%20d%20e%20f%20g%20h%20ignore%20this"),
+            ("ihavenospaces", "ihavenospaces"),
+            ("nospacesIgnoreme", "nospacesIgnoreme")
+        ]
         for args, expected in cases:
-            self.assertEqual(f(*args), expected, msg=args)
+            self.assertEqual(f1(*args), expected, msg=args)
+        for s, expected in cases_no_true_length:
+            self.assertEqual(f2(s), expected, msg=s)
 
     def test_urlify(self):
-        self._run_tests(urlify)
+        self._run_tests(urlify, urlify_no_true_length)
 
 
 if __name__ == '__main__':
