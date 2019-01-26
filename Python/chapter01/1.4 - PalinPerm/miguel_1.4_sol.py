1be22cd53b5bd4f894a552382ff6e18eaa80c931
miguelhx <mhernandez401405@gmail.com>
2019-01-26 15:54:10 -0800

    miguel solution to 1.4 palindrome perm

---
+++ Python/chapter01/1.4 - PalinPerm/miguel_1.4_sol.py
@@ -0,0 +1,82 @@
+"""
+Python version 3.7.0
+1.4 - Palindrome Permutation
+Given a string, write a function to check if it is a permutation of a palindrome.
+A palindrome is a word or phrase that is the same forwards and backwards.  A permutation is a
+rearrangement of letters.  The palindrome does not need to be limited to just dictionary words.
+"""
+import unittest
+import itertools as it
+
+
+def _is_palindrome(word: str) -> bool:
+    """
+    Checks if word is a palindrome.
+    A palindrome is a word or phrase that is the same forwards and backwards.
+    Whitespace will not be considered when determining palindrome.
+    This function is case insensitive.
+    :param word: the word we check
+    :return: true if word is a palindrome, false otherwise
+    """
+    word_no_spaces = word.replace(' ', '').lower()
+    reversed_word = word_no_spaces[::-1]
+    return word_no_spaces == reversed_word
+
+
+def is_permutation_of_palindrome(word: str) -> bool:
+    """
+    Given a string, this function will return whether the string is a permutation of a palindrome.
+    A palindrome is a word or phrase that is the same forwards and backwards.
+    A permutation is a rearrangement of letters.
+    When evaluating whether a string is the same forwards and backwards, we will
+    omit whitespace.  For ex: "taco cat" would not equal "tac ocat" IF we expect a space at the 4th index for
+    the reversed version.  So, the space will not be taken into account only when determining palindrome.
+    Assuming ASCII
+    Runtime is O(n!)
+    Space complexity is O(n!)
+    Given:      Expect:
+    Tact Coa        True (permutations: "taco cat", "atco cta")
+
+    :param word: the string that we want to check for perm of a palindrome
+    :return: True if word is a palindrome, False otherwise.
+    """
+    word_no_spaces = word.replace(' ', '')
+    perms = [''.join(p) for p in it.permutations(word_no_spaces)]
+    for p in perms:
+        if _is_palindrome(p):
+            return True
+    return False
+
+
+class TestIsPermutationOfPalindromeFunction(unittest.TestCase):
+    def test_is_permutation_of_palindrome(self):
+        cases = [
+            ("Tact Coa", True),
+            ("car race", True),
+            ("ppilffli", True),
+            ("gwas", False),
+            ("sldkjflksd", False),
+            (" ", True),
+            ("", True),
+            ("a", True)
+        ]
+        for word, expected in cases:
+            self.assertEqual(is_permutation_of_palindrome(word), expected, msg=word)
+
+    def test_is_palindrome(self):
+        cases = [
+            ("Taco Cat", True),
+            ("race car", True),
+            ("flippilf", True),
+            ("swag", False),
+            ("miguel", False),
+            (" ", True),
+            ("", True),
+            ("a", True)
+        ]
+        for word, expected in cases:
+            self.assertEqual(_is_palindrome(word), expected, msg=word)
+
+
+if __name__ == '__main__':
+    unittest.main()
