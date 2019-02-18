4f308e2d41cf7bd90a850353bf85ecedc596b23a
miguelhx <mhernandez401405@gmail.com>
2019-02-17 22:57:45 -0800

    one away soln update

--- Python/chapter01/1.5 - OneAway/miguel_1.5_sol.py
+++ Python/chapter01/1.5 - OneAway/miguel_1.5_sol.py
@@ -6,7 +6,6 @@ remove a character, or replace a character.  Given two strings, write a function
 if they are one edit (or zero edits) away.
 """
 import unittest
-import collections
 from typing import Callable
 
 
@@ -18,27 +17,43 @@ def one_away(s1: str, s2: str) -> bool:
     2. remove a character
     3. replace a character
     Zero edits away means the two strings are exactly the same.
-    We will analyze character frequencies to detect anomalies.
-    An anomaly can be any of the 3 types of edits.
-    Runtime:  O(n + m), where n is length of s1 and m is length of s2
-    Space Complexity:  O(n + m)
+    We will do different checks depending on the size different of the strings.
+    Runtime:  O(n), where n is length of s1
+    Space Complexity:  O(n + n +/- 1) = O(n), where n is length of s1
     :param s1: one string we want to compare
     :param s2: other string to compare
     :return: true if one or zero edits away, false otherwise
     """
-    # if size differs by more than 1, guaranteed more than one anomaly.
+    # if size differs by more than 1, guaranteed more than one edit distance.
     if abs(len(s1) - len(s2)) > 1:
         return False
+    # if size is the same, scan for edits
+    if len(s1) == len(s2):
+        edits = 0
+        for i, c in enumerate(s1):
+            if c == s2[i]:
+                continue
+            edits += 1
+            if edits > 1:
+                return False
+        return True
+    # otherwise, we have an insertion/deletion
+    # compare both strings for insertion/deletion
+    # want to loop through the shorter string to avoid going out of bounds
+    s_short = s1 if len(s1) < len(s2) else s2
+    s_long = s1 if len(s1) > len(s2) else s2
 
-    char_freqs_s1 = collections.Counter(s1)
-    char_freqs_s2 = collections.Counter(s2)
-
-    anomalies = 0
-    for key, value in char_freqs_s1.items():
-        if key not in char_freqs_s2 or value != char_freqs_s2[key]:
-            anomalies += 1
-        if anomalies > 1:
-            return False
+    addend = 0
+    for i, c in enumerate(s_short):
+        if c == s_long[i + addend]:
+            continue
+        if i == i + addend:
+            # addend is 0, will check next char in next iteration
+            addend += 1
+            continue
+        # otherwise, addend is not 0, and we did not match characters.
+        # guaranteed at least 2 edit distance
+        return False
     return True
 
 
@@ -56,6 +71,8 @@ class TestOneAwayFunction(unittest.TestCase):
             ("a", "b", True),
             ("", "", True),
             ("pale", "elap", False),
+            ("pale", "elaps", False),
+            ("pale", "palse", True)
         ]
         for s1, s2, expected in cases:
             self.assertEqual(f(s1, s2), expected, msg=(s1, s2))
