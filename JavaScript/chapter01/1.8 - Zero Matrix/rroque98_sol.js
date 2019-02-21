b011b224a80afafedaa5b48db153bef77f0a1e40
Ricardo <rroque98@gmail.com>
2019-02-21 15:02:50 -0800

    Refactor 1.8 Zero Matrix solution for readability

--- JavaScript/chapter01/1.8 - Zero Matrix/rroque98_sol.js
+++ JavaScript/chapter01/1.8 - Zero Matrix/rroque98_sol.js
@@ -2,18 +2,16 @@
  in an M*N matrix is 0,it's entire row and column
   are set to 0*/
 
-const zeroMatrix = (nestedArr) => {
-  var rowsAndColsWithZeros = checkForZeroIndex(nestedArr);
-  let rowLength = nestedArr[0].length;
-  let columnLength = nestedArr.length;
-  for (let i = 0; i < rowsAndColsWithZeros['rows'].length; i++) {
-    let rowIndex = rowsAndColsWithZeros['rows'][i];
+const zeroMatrix = nestedArr => {
+  const rowsAndColsWithZeros = checkForZeroIndex(nestedArr);
+  const rowLength = nestedArr[0].length;
+  const columnLength = nestedArr.length;
+  for (const rowIndex of rowsAndColsWithZeros['rows']) {
     for (let x = 0; x < rowLength; x++) {
       nestedArr[rowIndex][x] = 0;
     }
   }
-  for (let i = 0; i < rowsAndColsWithZeros['columns'].length; i++) {
-    let colIndex = rowsAndColsWithZeros['columns'][i];
+  for (const colIndex of rowsAndColsWithZeros['columns']) {
     for (let x = 0; x < columnLength; x++) {
       nestedArr[x][colIndex] = 0;
     }
@@ -22,7 +20,7 @@ const zeroMatrix = (nestedArr) => {
 
   // **** Helper functions ****
   function checkForZeroIndex(nestArr) {
-    let zeroIndices = {rows: [], columns: []};
+    let zeroIndices = { rows: [], columns: [] };
     for (let i = 0; i < nestArr.length; i++) {
       for (let x = 0; x < nestArr[i].length; x++) {
         if (nestArr[i][x] === 0) {
@@ -33,52 +31,25 @@ const zeroMatrix = (nestedArr) => {
     }
     return zeroIndices;
   }
-}
+};
 
 // **** TESTS ****:
-
-var testArr1 = [
-  [3, 5, 6],
-  [1, 0, 2],
-  [4, 4, 5],
-  [2, 2, 2],
-]
-
-var resultArr1 = [
-  [3, 0, 6],
-  [0, 0, 0],
-  [4, 0, 5],
-  [2, 0, 2],
-]
-
-var testArr2 = [
-  [3, 5, 6],
-  [1, 0, 2],
-  [4, 4, 0],
-  [2, 0, 2],
-]
-
-var resultArr2 = [
-  [3, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-]
-
-var testArr3 = [
-  [3, 5, 6],
-  [0, 0, 2],
-  [4, 4, 0],
-  [2, 0, 2],
-]
-
-var resultArr3 = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-]
-console.log(JSON.stringify(zeroMatrix(testArr1)) === JSON.stringify(resultArr1));
-console.log(JSON.stringify(zeroMatrix(testArr2)) === JSON.stringify(resultArr2));
-console.log(JSON.stringify(zeroMatrix(testArr3)) === JSON.stringify(resultArr3));
 console.log(JSON.stringify(zeroMatrix([[]])) === JSON.stringify([[]]));
+
+const testArr1 = [[3, 5, 6], [1, 0, 2], [4, 4, 5], [2, 2, 2]];
+const resultArr1 = [[3, 0, 6], [0, 0, 0], [4, 0, 5], [2, 0, 2]];
+console.log(
+  JSON.stringify(zeroMatrix(testArr1)) === JSON.stringify(resultArr1)
+);
+
+const testArr2 = [[3, 5, 6], [1, 0, 2], [4, 4, 0], [2, 0, 2]];
+const resultArr2 = [[3, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+console.log(
+  JSON.stringify(zeroMatrix(testArr2)) === JSON.stringify(resultArr2)
+);
+
+const testArr3 = [[3, 5, 6], [0, 0, 2], [4, 4, 0], [2, 0, 2]];
+const resultArr3 = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+console.log(
+  JSON.stringify(zeroMatrix(testArr3)) === JSON.stringify(resultArr3)
+);
