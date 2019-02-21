b15908ad6e518543ab35171ac0a06adaf8ecdc59
Ricardo <rroque98@gmail.com>
2019-02-21 15:50:22 -0800

    Refactor 1.7 Rotate Matrix due to error in initial test logic

--- JavaScript/chapter01/1.7 - Rotate Matrix/rroque98_sol.js
+++ JavaScript/chapter01/1.7 - Rotate Matrix/rroque98_sol.js
@@ -3,22 +3,27 @@ each pixel in the image is 4 bytes, write a method
 to rotate the image by 90 degrees. Can you do this
 in place?*/
 
-const rotateImage = (nestedArr) => {
-  if (nestedArr.length === 0) {
-    return [];
+const rotateImage = nestedArr => {
+  const n = nestedArr.length;
+  if (n === 0 || n === 1) {
+    return nestedArr;
   }
   var rotatedArr = [];
-  var numOfRows = nestedArr[0].length;
-  for (let x = 0; x < numOfRows; x++) {
+  for (let col = 0; col < n; col++) {
     let newRow = [];
-    for (let i = nestedArr.length - 1; i >= 0; i--) {
-      newRow.push(nestedArr[i][x]);
+    for (let row = n - 1; row >= 0; row--) {
+      newRow.push(nestedArr[row][col]);
     }
     rotatedArr.push(newRow);
   }
   return rotatedArr;
-}
+};
 
 // TESTS:
-console.log(JSON.stringify(rotateImage([[1,2,3],[4,5,6]])) === JSON.stringify([[4,1],[5,2],[6,3]]));
+console.log(
+  JSON.stringify(rotateImage([[1, 2, 3], [4, 5, 6], [7, 8, 9]])) ===
+    JSON.stringify([[7, 4, 1], [8, 5, 2], [9, 6, 3]])
+);
+console.log(JSON.stringify(rotateImage([[1]])) === JSON.stringify([[1]]));
+console.log(JSON.stringify(rotateImage([[]])) === JSON.stringify([[]]));
 console.log(JSON.stringify(rotateImage([])) === JSON.stringify([]));
