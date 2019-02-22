a8c900680babbd09d9c2897e0182da2a10884b18
Ricardo <rroque98@gmail.com>
2019-02-21 19:37:09 -0800

    Refactor 1.5 OneAway solution to improve readability and missing tests

--- JavaScript/chapter01/1.5 - OneAway/rroque98_sol.js
+++ JavaScript/chapter01/1.5 - OneAway/rroque98_sol.js
@@ -4,65 +4,65 @@ Given 2 strings write a function that will check if it is
 1 or 0 edits away*/
 
 const isOneAway = (str1, str2) => {
+  const diffInLen = Math.abs(str1.length - str2.length);
+  if (diffInLen > 1) {
+    return false;
+  }
+
+  let errorCount = 0;
   if (str1.length === str2.length) {
-    let errorCount = 0;
     for (let i = 0; i < str1.length; i++) {
-      if (str1[i] === str2[i]) {
-        continue;
-      }
-      errorCount++;
-      if (errorCount > 1) {
-        return false;
+      if (str1[i] !== str2[i]) {
+        errorCount++;
+        if (errorCount > 1) {
+          return false;
+        }
       }
     }
   } else {
-    let errorCount = 0;
-    const longestStr = findLongestStr(str1, str2);
-    let x = 0;
-    for(let i = 0; i < longestStr.length; i++) {
-      let currentChar1 = str1[i];
-      let currentChar2 = str2[x]
-      let nextChar1 = str1[i + 1];
-      let nextChar2 = str2[x + 1];
-      if (str1[i] === str2[x]) {
-        x++;
-        continue;
+    const longStr = str1.length > str2.length ? str1 : str2;
+    const shortStr = str1.length < str2.length ? str1 : str2;
+    for (
+      let i = 0, x = 0;
+      i < longStr.length && x < shortStr.length;
+      i++, x++
+    ) {
+      if (longStr[i] !== shortStr[x]) {
+        errorCount++;
+        if (errorCount > 1) {
+          return false;
+        }
+        i++;
       }
-      errorCount++;
-      if (errorCount > 1) {
-        return false;
-      }
-      if (currentChar1 === nextChar2) {
-        x++;
-        i--;
-        continue;
-      } else if (currentChar2 === nextChar1) {
-        continue;
-      }
-      errorCount++;
-      return false;
-    }
-    // ***** Helper functions ********
-    function findLongestStr(str1, str2) {
-      return (str1.length > str2.length ? str1 : str2);
     }
   }
   return true;
-}
+};
 
 // TESTS
-console.log(isOneAway('pale', 'ple') === true);
-console.log(isOneAway('pales', 'pale') === true);
-console.log(isOneAway('pale', 'bale') === true);
-console.log(isOneAway('pale', 'bake') === false);
-console.log(isOneAway('paleo', 'palseo') === true);
+console.log(isOneAway('pale', 'ple') === true); // deletion
+console.log(isOneAway('pale', 'opale') === true); // insertion in beginning
+console.log(isOneAway('pale', 'palse') === true); // insertion in middle
+console.log(isOneAway('pale', 'pales') === true); // insertion at end
+console.log(isOneAway('pale', 'bale') === true); // replacement
+console.log(isOneAway('pale', 'ae') === false); // greater than 1 deletions
+console.log(isOneAway('pale', 'ppalpe') === false); // greater than 1 insertions
+console.log(isOneAway('pale', 'bake') === false); // greater than 1 replacements
+console.log(isOneAway('pale', 'balpe') === false); // 1 insertion, 1 replacement
+console.log(isOneAway('pale', 'plo') === false); // 1 deletion, 1 replacement
+console.log(isOneAway('pale', 'ales') === false); // 1 deletion, 1 insertion
+// swap str1 with str2
+console.log(isOneAway('ple', 'pale') === true); // deletion
+console.log(isOneAway('opale', 'pale') === true); // insertion in beginning
+console.log(isOneAway('palse', 'pale') === true); // insertion in middle
+console.log(isOneAway('pales', 'pale') === true); // insertion at end
+console.log(isOneAway('bale', 'pale') === true); // replacement
+console.log(isOneAway('ae', 'pale') === false); // greater than 1 deletions
+console.log(isOneAway('ppalpe', 'pale') === false); // greater than 1 insertions
+console.log(isOneAway('bake', 'pale') === false); // greater than 1 replacements
+console.log(isOneAway('balpe', 'pale') === false); // 1 insertion, 1 replacement
+console.log(isOneAway('plo', 'pale') === false); // 1 deletion, 1 replacement
+console.log(isOneAway('ales', 'pale') === false); // 1 deletion, 1 insertion
 console.log(isOneAway('p', 'b') === true);
 console.log(isOneAway('', '') === true);
 console.log(isOneAway('p', 'p') === true);
-console.log(isOneAway('pale', 'ppalpe') === false);
-
-console.log(isOneAway('ple', 'pale') === true);
-console.log(isOneAway('pale', 'pales') === true);
-console.log(isOneAway('bale', 'pale') === true);
-console.log(isOneAway('bake', 'pale') === false);
-console.log(isOneAway('palseo', 'paleo') === true);
