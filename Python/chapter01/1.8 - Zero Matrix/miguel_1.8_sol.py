870abc8985b39e2624d120ca38395b6916021546
miguelhx <mhernandez401405@gmail.com>
2019-06-01 22:46:57 -0700

    update to new solution

--- Python/chapter01/1.8 - Zero Matrix/miguel_1.8_sol.py
+++ Python/chapter01/1.8 - Zero Matrix/miguel_1.8_sol.py
@@ -10,32 +10,33 @@ from typing import List
 def zero_matrix(matrix: List[List[int]]) -> List[List[int]]:
     """
     zero_matrix will take in an MxN matrix and when a 0 element is found, the whole column and row
-    will be set to 0.
+    will be set to 0. The algorithm works by first scanning for coordinates where there are 0s and
+    storing those coordinates.
+    Then we go through each coordinate and set the row and column at that coordinate equal to 0.
     Runtime: O(M * N)
-    Space Complexity: O(min(M, N))
+    Space Complexity: O(M * N) - worst case, input matrix has all zero values
     :param matrix: an MxN matrix.  M is the number of rows, N is the number of columns
     :return: a zero-modified matrix
     """
     M = len(matrix)
     N = len(matrix[0])
 
-    zeroed_rows = set()
-    zeroed_cols = set()
-
+    zero_coordinates = []
+    # first, scan for coordinates of 0s
     for i, row in enumerate(matrix):
         for j, num in enumerate(row):
-            if num != 0 or i in zeroed_rows or j in zeroed_cols:
+            if num != 0:
                 continue
-            # otherwise, set row to 0 by looping through columns of current row
-            for k in range(N):
-                matrix[i][k] = 0
-            # set column to 0 by looping through rows of current column
-            for l in range(M):
-                matrix[l][j] = 0
-            # update zeroed row and col sets
-            zeroed_cols.add(j)
-            zeroed_rows.add(i)
-            break
+            # otherwise, save the coordinate
+            zero_coordinates.append((i, j))
+    # then, set row and col to zero of each coordinate
+    for i, j in zero_coordinates:
+        # set row to 0 by looping through columns of current row
+        for k in range(N):
+            matrix[i][k] = 0
+        # set column to 0 by looping through rows of current column
+        for l in range(M):
+            matrix[l][j] = 0
     return matrix
 
 
