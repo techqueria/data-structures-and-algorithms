81287c3a2f12de228a1e2b6b1e75a1769bb30631
miguelhx <mhernandez401405@gmail.com>
2019-07-01 13:13:24 -0700

    add 2 more test cases

--- Python/chapter02/2.4 - Partition/miguel_2.4_soln.py
+++ Python/chapter02/2.4 - Partition/miguel_2.4_soln.py
@@ -214,6 +214,16 @@ class TestPartition(unittest.TestCase):
                 LinkedList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1),
                 1,
                 True
+            ),
+            (
+                LinkedList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1),
+                5,
+                False
+            ),
+            (
+                LinkedList(1, 2, 3, 4, 5, 6, 7, 8, 9),
+                5,
+                True
             )
         ]
 
