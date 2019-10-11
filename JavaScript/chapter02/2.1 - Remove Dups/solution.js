e0006925c313e93e3bce23f9e0da6e234bedfb30
aditya9061 <adiajg20@gmail.com>
2019-10-11 23:29:52 +0530

    Rectified some errors.

--- JavaScript/chapter02/2.1 - Remove Dups/solution.js
+++ JavaScript/chapter02/2.1 - Remove Dups/solution.js
@@ -16,29 +16,29 @@ class LinkedList{
 	}
 
 //Function to insert a node in the singlely linkedlist.
-	insert=(data)=>{
+	insert(data){
 		
 		var newnode=new node(data);
-		if(head===null){
+		if(this.head==null){
 			this.head=newnode;
 		}
 
 		else{
 			
 			var temp=this.head;
-			while(temp.next!==null){
+			while(temp.next){
 				temp=temp.next;			
 			}		
 				
-			temp=newnode;
+			temp.next=newnode;
 		}	
 	}
 
 //Function to print the singlely linkedlist.
-	printList=()=>{
+	printList(){
 		var temp=this.head;
-		var list;
-		while(temp!==null){
+		var list="";
+		while(temp){
 			list+=temp.data+"->";
 			temp=temp.next;		
 		}
@@ -49,19 +49,16 @@ class LinkedList{
 	
 
 //Function to remove dupliicate nodes from the singlely linkedlist.
-	removeDuplicates=()=>{
+	removeDuplicates(){
 	
-		var temp1,temp2,duplicate;
+		var temp1,temp2;
 	
 		temp1=this.head;
-		temp2=temp1;
-		while(temp1!==null || temp.next!==null){
-			
-			while(temp2.next!==null){
-				if(temp1.data===temp2.next.data){
-					duplicate=temp2.next;
+		while(temp1 && temp1.next){
+			temp2=temp1;
+			while(temp2.next){
+				if(temp1.data==temp2.next.data){
 					temp2.next=temp2.next.next;
-					delete(duplicate);
 				}
 				else{
 					temp2=temp2.next;				
@@ -73,21 +70,18 @@ class LinkedList{
 	}
 
 }
-
 //main function.
-/*
-Linkedlist list;
-list.insert(0);
-list.insert(1);
-list.insert(2);
-list.insert(3);
-list.insert(1);
-list.insert(4);
-list.insert(4);
-console.log("Original List with DUplicates:");
-list.printList();
-list.removeDuplicates();
-console.log("List free of duplicates:");
-list.printList();
+/*var list_1=new LinkedList();
+list_1.insert(0);
+list_1.insert(1);
+list_1.insert(2);
+list_1.insert(3);
+list_1.insert(1);
+list_1.insert(4);
+list_1.insert(4);
+console.log("Original List with Duplicates:");
+list_1.printList();
+list_1.removeDuplicates();
+console.log("New List free of duplicates:");
+list_1.printList();
 */
-
