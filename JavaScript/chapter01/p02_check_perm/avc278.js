85f7e6e4cd194f266abdde7abfaca5edce5ae850
Andres Callanaupa <avc278@nyu.edu>
2020-07-31 10:14:45 -0400

    PR changes, and added a new solution

--- JavaScript/chapter01/p02_check_perm/avc278.js
+++ JavaScript/chapter01/p02_check_perm/avc278.js
@@ -15,8 +15,7 @@ const assert = require("assert");
  * At this point, we can get the keys from the frequency objects we created, which is an O(N) operation for space.
  * If the lengths of `aKeys` and `bKeys` are not the same, we know they cannot be permutations and we exit early.
  * Otherwise, we can continue to iterate through the keys. We need to make sure that the frequencies of aKeys exist in
- * both aFreq and bFreq AND that the frequencies of bKeys exist in aFreq and bFreq. It is not enough to check that one
- * exists in both and not check the other set of keys. If at any point, they do not match, we can return false.
+ * both aFreq and bFreq. If at any point, they do not match, we can return false.
  *
  * If we haven't returned false up to this point, it's safe to return true.
  *
@@ -24,16 +23,15 @@ const assert = require("assert");
  * Space:   O(N)
  *
  */
-const checkPermutation = (A, B) => {
+const checkPermutation1 = (A, B) => {
   if (A.length !== B.length) return false;
   const aFreq = {};
   const bFreq = {};
-  for (let i = 0; i < A.length; i++) {
-    aFreq[A[i]] = aFreq[A[i]] || 0;
-    aFreq[A[i]] += 1;
-
-    bFreq[B[i]] = bFreq[B[i]] || 0;
-    bFreq[B[i]] += 1;
+  for (const a of A) {
+    aFreq[a] = (aFreq[a] || 0) + 1;
+  }
+  for (const b of B) {
+    bFreq[b] = (bFreq[b] || 0) + 1;
   }
 
   const aKeys = Object.keys(aFreq);
@@ -41,31 +39,60 @@ const checkPermutation = (A, B) => {
   if (aKeys.length !== bKeys.length) return false;
   for (let i = 0; i < aKeys.length; i++) {
     if (aFreq[aKeys[i]] !== bFreq[aKeys[i]]) return false;
-    if (aFreq[bKeys[i]] !== bFreq[bKeys[i]]) return false;
   }
 
   return true;
+};
+
+/**
+ * Checks if a string is a permutation of another
+ * @param  {string} A source input string to check against
+ * @param  {string} B target input string to compare with source string
+ * @return {bool}     Whether the input strings are permutations of one another
+ *
+ * An alternative to this problem, and significantly more elegant, is to simply split the input strings into arrays,
+ * use the built-in sort array method, then join back to a string, and check if A and B are equal.
+ * The trade off is since we sort arrays of length N, the runtime becomes slower from O(N) to O(NlogN).
+ * As for space, we create arrays of length N, so there is no change to space performance compared to checkPermutation1.
+ * Even though this is less performant to checkPermutation1, for production code, this seems to be more maintainable
+ * and way easier to read at a glance.
+ *
+ * Runtime: O(NlogN)
+ * Space:   O(N)
+ *
+ */
+const checkPermutation2 = (A, B) => {
+  if (A.length !== B.length) return false;
+
+  A = A.split("").sort().join("");
+  B = B.split("").sort().join("");
+
+  return A === B;
 };
 
 describe(module.filename, () => {
   it("should return false on input strings not of the same size", () => {
     const A = "hello";
     const B = "hi";
-    assert.equal(checkPermutation(A, B), false);
+    assert.equal(checkPermutation1(A, B), false);
+    assert.equal(checkPermutation2(A, B), false);
   });
   it("should return false on input strings whose key lengths do not match", () => {
     const A = "abcd";
     const B = "abcc";
-    assert.equal(checkPermutation(A, B), false);
+    assert.equal(checkPermutation1(A, B), false);
+    assert.equal(checkPermutation2(A, B), false);
   });
   it("should return false on input strings whose key frequencies do not match", () => {
     const A = "abccd";
     const B = "abcce";
-    assert.equal(checkPermutation(A, B), false);
+    assert.equal(checkPermutation1(A, B), false);
+    assert.equal(checkPermutation2(A, B), false);
   });
   it("should return true on input strings whose letter frequencies match", () => {
     const A = "racecar";
     const B = "aaccerr";
-    assert.equal(checkPermutation(A, B), true);
+    assert.equal(checkPermutation1(A, B), true);
+    assert.equal(checkPermutation2(A, B), true);
   });
 });
