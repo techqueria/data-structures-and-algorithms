899b7744040bfcea8525afc8399f9a65a3438f0c
Andres Callanaupa <avc278@nyu.edu>
2020-08-15 16:46:53 -0400

    2.3 solution

---
+++ JavaScript/chapter02/p03_delete_middle_node/avc278.js
@@ -0,0 +1,67 @@
+// Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e. any node but the first and last node,
+// not necessarily the exact middle) of a singly linked list, given only access to that node.
+// EXAMPLE:
+// Input: the node c from the linked list a => b => c => d => e => f
+// Result: nothing is returned, but the new linked list looks like a => b => d => e => f
+
+const assert = require("assert");
+
+class LinkedListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? null : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const arrayToLinkedList = (arr) => {
+  let tail = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    tail = new LinkedListNode(arr[i], tail);
+  }
+  return tail;
+};
+
+const compareLinkedLists = (A, B) => {
+  if (!A && !B) return true;
+
+  while (A !== null && B !== null) {
+    if (A.val !== B.val) return false;
+    A = A.next;
+    B = B.next;
+  }
+  console.log(A, B);
+  return !((A && !B) || (!A && B));
+};
+/**
+ * Deletes an inputted node somewhere in the middle of the linked list
+ * @param  {LinkedListNode} node input node to be removed
+ * @return {null}
+ *
+ * There's no trick to this problem. We know that a linked list, fundamentally, is just a collection of nodes with
+ * values that point to another node. We don't need to necessarily delete the selected node. We just need to overwrite
+ * what that node represents. In other words, we can overwrite the next node onto the current node, then skip from the
+ * current node to the next next node, effectively skipping over the node after the selected node since it's redundant.
+ * Runtime: O(1)
+ * Space:   O(1)
+ *
+ */
+const deleteMiddleNode = (node) => {
+  // It is safe to assume from the prompt that the given node exists in the linked list and is not the head nor tail
+  const next = node.next;
+  node.val = next.val;
+  node.next = next.next;
+};
+
+describe(module.filename, () => {
+  it("should remove a node in the middle of the linked list", () => {
+    const arr = [1, 2, 3, 4, 5];
+    let ll1 = arrayToLinkedList(arr);
+    const nodeToBeRemoved = ll1.next.next.next;
+    const expectedArr = [1, 2, 3, 5];
+    let expectedLL1 = arrayToLinkedList(expectedArr);
+
+    deleteMiddleNode(nodeToBeRemoved);
+
+    assert.ok(compareLinkedLists(ll1, expectedLL1));
+  });
+});
