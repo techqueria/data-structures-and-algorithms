3aecceece19b043e8388adda18de08771c9d8041
Andres Callanaupa <avc278@nyu.edu>
2020-08-21 15:17:31 -0400

    PR changes. Changed logic to actually return the linked list at the desired node

--- JavaScript/chapter02/p04_partition/avc278.js
+++ JavaScript/chapter02/p04_partition/avc278.js
@@ -14,7 +14,7 @@ const {
 
 /**
  * A destructive function, `partition`, splits a linked list based on the input `partitionValue`, with lesser values to
- * the left and greater values to the right.
+ * the left and greater values to the right, and returns the partitioned linked list.
  * @param   {LinkedListNode} head input linked list to be partitioned
  * @return  {LinkedListNode}      partitioned linked list
  *
@@ -36,6 +36,7 @@ const partition = (head, partitionValue) => {
   let moreTail;
 
   while (head !== null) {
+    const next = head.next;
     if (head.val < partitionValue) {
       if (!lessTail) {
         lessTail = head;
@@ -53,17 +54,20 @@ const partition = (head, partitionValue) => {
         moreTail = moreTail.next;
       }
     }
-    head = head.next;
+    head = next;
   }
 
   if (!lessHead && moreHead) {
-    head = moreHead;
-  } else if (lessHead && !moreHead) {
-    head = lessHead;
-  } else {
-    lessTail.next = moreHead;
-    moreTail.next = null;
+    return moreHead;
   }
+
+  if (lessHead && !moreHead) {
+    return lessHead;
+  }
+
+  lessTail.next = moreHead;
+  moreTail.next = null;
+  return lessHead;
 };
 
 describe(module.filename, () => {
@@ -73,9 +77,9 @@ describe(module.filename, () => {
     const expectedArr = [3, 2, 1, 5, 8, 5, 10];
     const expectedPartition = arrayToLinkedList(expectedArr);
 
-    partition(ll, 5);
+    const partitionedLL = partition(ll, 5);
 
-    assert.ok(compareLinkedLists(ll, expectedPartition));
+    assert.ok(compareLinkedLists(partitionedLL, expectedPartition));
   });
   it("should return the partitioned linked list when the partition value does not exist in the linked list", () => {
     const arr = [1, 9, 3, 8, 6, 7, 4, 2, 10];
@@ -83,9 +87,19 @@ describe(module.filename, () => {
     const expectedArr = [1, 3, 4, 2, 9, 8, 6, 7, 10];
     const expectedPartition = arrayToLinkedList(expectedArr);
 
-    partition(ll, 5);
+    const partitionedLL = partition(ll, 5);
 
-    assert.ok(compareLinkedLists(ll, expectedPartition));
+    assert.ok(compareLinkedLists(partitionedLL, expectedPartition));
+  });
+  it("should return the partitioned linked list when the partition flips the array", () => {
+    const arr = [10, 1];
+    const ll = arrayToLinkedList(arr);
+    const expectedArr = [1, 10];
+    const expectedPartition = arrayToLinkedList(expectedArr);
+
+    const partitionedLL = partition(ll, 5);
+
+    assert.ok(compareLinkedLists(partitionedLL, expectedPartition));
   });
   it("should return the partitioned linked list when the partition value is less than all values in the array", () => {
     const arr = [10];
@@ -93,9 +107,9 @@ describe(module.filename, () => {
     const expectedArr = [10];
     const expectedPartition = arrayToLinkedList(expectedArr);
 
-    partition(ll, 5);
+    const partitionedLL = partition(ll, 5);
 
-    assert.ok(compareLinkedLists(ll, expectedPartition));
+    assert.ok(compareLinkedLists(partitionedLL, expectedPartition));
   });
   it("should return the partitioned linked list when the partition value is more than all values in the array", () => {
     const arr = [1];
@@ -103,8 +117,8 @@ describe(module.filename, () => {
     const expectedArr = [1];
     const expectedPartition = arrayToLinkedList(expectedArr);
 
-    partition(ll, 5);
+    const partitionedLL = partition(ll, 5);
 
-    assert.ok(compareLinkedLists(ll, expectedPartition));
+    assert.ok(compareLinkedLists(partitionedLL, expectedPartition));
   });
 });
