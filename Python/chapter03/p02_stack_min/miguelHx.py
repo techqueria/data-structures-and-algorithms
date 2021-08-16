64d2effe52c84e024dc164a6e2b2e9356d485e21
miguelhx <mhernandez401405@gmail.com>
2021-08-16 00:07:04 -0700

    Update MyStack changes from 3.1 branch

--- Python/chapter03/p02_stack_min/miguelHx.py
+++ Python/chapter03/p02_stack_min/miguelHx.py
@@ -16,7 +16,6 @@ from typing import List, Optional, Iterator
 
 T = TypeVar('T')
 
-
 @dataclass
 class StackNode(Generic[T]):
     data: T
@@ -24,19 +23,20 @@ class StackNode(Generic[T]):
     running_min: int = sys.maxsize
 
 class MyStack(object):
-    """
-    Stack data structure implementation.
+    """Stack data structure implementation.
     Uses LIFO (last-in first-out) ordering.
     The most recent item added to the stack is
     the first removed.  Traversal is top to bottom.
     """
 
     def __init__(self):
-        self.top = None # top is a pointer to StackNode object
-        self.size = 0
+        self.top: Optional[StackNode[T]] = None # top is a pointer to StackNode object
+        self.size: int = 0
+        self.index: int = -1 # for iterator use
+        self.current_node: Optional[StackNode[T]] = self.top # also for iterator use
         return
     
-    def pop(self) -> int:
+    def pop(self) -> T:
         """
         Removes the top item from the stack
 
@@ -50,20 +50,22 @@ class MyStack(object):
             raise IndexError('Stack is Empty.')
         item = self.top.data
         self.top = self.top.next
+        self.current_node = self.top
         self.size -= 1
         return item
     
-    def push(self, item: int) -> None:
+    def push(self, item: T) -> None:
         """
         Adds an item to the top of the stack
         Args:
             item (int): data we want at the top of stack
         """
-        t = StackNode(item)
+        t = StackNode(item, None)
         t.next = self.top
         if self.top is None:
             t.running_min = item
             self.top = t
+            self.current_node = self.top
             self.size += 1
             # print("Stack Node data = {}, local min = {}".format(item, t.running_min))
             return
@@ -73,9 +75,10 @@ class MyStack(object):
             t.running_min = self.top.running_min
         # print("Stack Node data = {}, local min = {}".format(item, t.running_min))
         self.top = t
+        self.current_node = self.top
         self.size += 1
 
-    def min(self) -> int:
+    def min(self) -> T:
         """
         Returns the element with the lowest value in the stack
 
@@ -84,7 +87,7 @@ class MyStack(object):
         """
         return self.top.running_min
 
-    def peek(self) -> int:
+    def peek(self) -> T:
         """
         Returns data at the top of the stack
 
@@ -98,17 +101,7 @@ class MyStack(object):
             raise IndexError('Stack is Empty')
         return self.top.data
     
-    def is_empty(self) -> bool:
-        """
-        Returns True if and only if the
-        stack is empty, otherwise False.
-
-        Returns:
-            bool: True if empty, False otherwise
-        """
-        return self.size == 0
-    
-    def as_list(self) -> List[int]:
+    def __iter__(self) -> Iterator:
         """
         Builds a list of the current stack state.
         For example, given the following stack:
@@ -118,13 +111,30 @@ class MyStack(object):
         Returns:
             List[int]: list of integers
         """
-        values = []
-        n = self.top
-        while n.next is not None:
-            values.append(n.data)
-            n = n.next
-        values.append(n.data)
-        return values
+        return self
+    
+    def __next__(self) -> T:
+        self.index += 1
+        if self.index == self.size or self.current_node is None:
+            self.index = -1
+            self.current_node = self.top
+            raise StopIteration
+        n: T = self.current_node.data
+        self.current_node = self.current_node.next
+        return n
+    
+    def __bool__(self) -> bool:
+        """
+        True is returned when the container is not empty.
+        From https://docs.python.org/3/reference/datamodel.html#object.__bool__ :
+        Called to implement truth value testing and the built-in operation bool();
+        should return False or True. When this method is not defined, len() is called,
+        if it is defined, and the object is considered true if its result is nonzero.
+        If a class defines neither len() nor bool(), all its instances are considered true.
+        Returns:
+            bool: False when empty, True otherwise
+        """
+        return self.size > 0
     
     def __str__(self):
         if self.size == 0:
@@ -155,9 +165,7 @@ class TestMyStack(unittest.TestCase):
         self.assertEqual(s.size, 3)
         self.assertEqual(s.top.data, 4)
         self.assertEqual(s.top.next.data, 3)
-
-        l = s.as_list()
-        self.assertEqual(l, [4, 3, 2])
+        self.assertEqual(list(s), [4, 3, 2])
     
     def test_stack_peek(self):
         s = MyStack()
@@ -169,12 +177,6 @@ class TestMyStack(unittest.TestCase):
         top_val = s.peek()
         self.assertEqual(top_val, 99)
     
-    def test_stack_is_empty(self):
-        s = MyStack()
-        self.assertTrue(s.is_empty())
-        s.push(7)
-        self.assertFalse(s.is_empty())
-    
     def test_stack_pop(self):
         # first case, attempt to pop an empty stack
         s = MyStack()
@@ -184,11 +186,11 @@ class TestMyStack(unittest.TestCase):
         s.push(2)
         s.push(3)
         # size is 3
-        self.assertEqual(s.as_list(), [3, 2, 1])
+        self.assertEqual(list(s), [3, 2, 1])
         val = s.pop()
         self.assertEqual(val, 3)
         self.assertEqual(s.size, 2) # size should now be 2
-        self.assertEqual(s.as_list(), [2, 1])
+        self.assertEqual(list(s), [2, 1])
     
     def test_stack_min(self):
         s = MyStack()
@@ -219,7 +221,13 @@ class TestMyStack(unittest.TestCase):
         s.pop()
         self.assertEqual(s.min(), 9)
         s.pop()
-        self.assertTrue(s.is_empty())
+        self.assertTrue(not s)
+    
+    def test__bool__(self):
+        s = MyStack()
+        self.assertFalse(s)
+        s.push(3)
+        self.assertTrue(s)
 
 
 if __name__ == '__main__':
