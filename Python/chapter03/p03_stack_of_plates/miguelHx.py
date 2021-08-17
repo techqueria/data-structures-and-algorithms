d0123d7595243256bb461b8f787722e6b51c6428
miguelhx <mhernandez401405@gmail.com>
2021-08-16 23:19:30 -0700

    Update MyStack implementation

--- Python/chapter03/p03_stack_of_plates/miguelHx.py
+++ Python/chapter03/p03_stack_of_plates/miguelHx.py
@@ -1,5 +1,4 @@
-"""
-Python version 3.7.0
+"""Python version 3.7.0
 3.3 - Stack of Plates
 Imagine a (literal) stack of plates.  If the stack gets too high, it might topple.
 Therefore, in real life, we would likely start a new stack when the previous stack
@@ -12,32 +11,36 @@ if there were just a single stack).
 FOLLOW UP:
 Implement a function popAt(int index) which performs a pop operation on a specific sub-stack.
 """
-
 import copy
 import unittest
-from typing import List
+
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+from typing import List, Optional, Iterator
+
+T = TypeVar('T')
+
+@dataclass
+class StackNode(Generic[T]):
+    data: T
+    next: 'Optional[StackNode[T]]'
 
 
-class StackNode(object):
-
-    def __init__(self, data):
-        self.data = data
-        self.next = None # next is a pointer to a StackNode object
-
-class MyStack(object):
-    """
-    Stack data structure implementation.
+class MyStack:
+    """Stack data structure implementation.
     Uses LIFO (last-in first-out) ordering.
     The most recent item added to the stack is
     the first removed.  Traversal is top to bottom.
     """
 
     def __init__(self):
-        self.top = None # top is a pointer to StackNode object
-        self.size = 0
+        self.top: Optional[StackNode[T]] = None # top is a pointer to StackNode object
+        self.size: int = 0
+        self.index: int = -1 # tracking current node for iterator use
+        self.current_node: Optional[StackNode[T]] = self.top
         return
 
-    def pop(self) -> int:
+    def pop(self) -> T:
         """
         Removes the top item from the stack
         Raises:
@@ -49,6 +52,7 @@ class MyStack(object):
             raise IndexError('Stack is Empty.')
         item = self.top.data
         self.top = self.top.next
+        self.current_node = self.top
         self.size -= 1
         return item
 
@@ -58,9 +62,10 @@ class MyStack(object):
         Args:
             item (int): data we want at the top of stack
         """
-        t = StackNode(item)
+        t = StackNode(item, None)
         t.next = self.top
         self.top = t
+        self.current_node = self.top
         self.size += 1
 
     def peek(self) -> int:
@@ -75,16 +80,7 @@ class MyStack(object):
             raise IndexError('Stack is Empty')
         return self.top.data
 
-    def is_empty(self) -> bool:
-        """
-        Returns True if and only if the
-        stack is empty, otherwise False.
-        Returns:
-            bool: True if empty, False otherwise
-        """
-        return self.size == 0
-
-    def as_list(self) -> List[int]:
+    def __iter__(self) -> Iterator:
         """
         Builds a list of the current stack state.
         For example, given the following stack:
@@ -94,13 +90,30 @@ class MyStack(object):
         Returns:
             List[int]: list of integers
         """
-        values = []
-        n = self.top
-        while n.next is not None:
-            values.append(n.data)
-            n = n.next
-        values.append(n.data)
-        return values
+        return self
+
+    def __next__(self) -> T:
+        self.index += 1
+        if self.index == self.size or self.current_node is None:
+            self.index = -1
+            self.current_node = self.top
+            raise StopIteration
+        n: T = self.current_node.data
+        self.current_node = self.current_node.next
+        return n
+
+    def __bool__(self) -> bool:
+        """
+        True is returned when the container is not empty.
+        From https://docs.python.org/3/reference/datamodel.html#object.__bool__ :
+        Called to implement truth value testing and the built-in operation bool();
+        should return False or True. When this method is not defined, len() is called,
+        if it is defined, and the object is considered true if its result is nonzero.
+        If a class defines neither len() nor bool(), all its instances are considered true.
+        Returns:
+            bool: False when empty, True otherwise
+        """
+        return self.size > 0
 
     def __str__(self):
         if self.size == 0:
@@ -131,8 +144,7 @@ class TestMyStack(unittest.TestCase):
         self.assertEqual(s.size, 3)
         self.assertEqual(s.top.data, 4)
         self.assertEqual(s.top.next.data, 3)
-
-        l = s.as_list()
+        l = list(s)
         self.assertEqual(l, [4, 3, 2])
 
     def test_stack_peek(self):
@@ -145,12 +157,6 @@ class TestMyStack(unittest.TestCase):
         top_val = s.peek()
         self.assertEqual(top_val, 99)
 
-    def test_stack_is_empty(self):
-        s = MyStack()
-        self.assertTrue(s.is_empty())
-        s.push(7)
-        self.assertFalse(s.is_empty())
-
     def test_stack_pop(self):
         # first case, attempt to pop an empty stack
         s = MyStack()
@@ -160,11 +166,18 @@ class TestMyStack(unittest.TestCase):
         s.push(2)
         s.push(3)
         # size is 3
-        self.assertEqual(s.as_list(), [3, 2, 1])
+        self.assertEqual(list(s), [3, 2, 1])
         val = s.pop()
         self.assertEqual(val, 3)
         self.assertEqual(s.size, 2) # size should now be 2
-        self.assertEqual(s.as_list(), [2, 1])
+        self.assertEqual(list(s), [2, 1])
+
+    def test__bool__(self):
+        s = MyStack()
+        self.assertFalse(s)
+        s.push(3)
+        self.assertTrue(s)
+
 
 
 if __name__ == '__main__':
