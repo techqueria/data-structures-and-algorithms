f01b2a2d7fe49dc6689cf765b801fbc0e941432f
miguelhx <mhernandez401405@gmail.com>
2021-08-28 17:42:48 -0700

    Implement route between nodes

--- Python/chapter04/p01_route_between_nodes/miguelHx.py
+++ Python/chapter04/p01_route_between_nodes/miguelHx.py
@@ -3,8 +3,6 @@
 Given a directed graph, design an algorithm to
 find out whether there is a route between two nodes.
 """
-
-
 import unittest
 
 from collections import deque
@@ -97,6 +95,51 @@ def bfs_search(root: Node) -> List[int]:
                 output.append(n.id)
     return output
 
+
+def route_between_nodes(src: Node, dest: Node) -> bool:
+    """This function will return true if a path
+    is found between two nodes, false otherwise.
+    The idea is to perform a breadth first search
+    from src to dest. After obtaining a list of
+    nodes visited, we simply check to see if destination
+    node id is in there.
+
+    Runtime Complexity:
+        O(V + E) where V represents the number of
+        nodes in the graph and E represents the number
+        of edges in this graph.
+    Space Complexity:
+        O(V) where V represents the number of existing nodes
+        in the graph.
+
+    Args:
+        src (Node): from node
+        dest (Node): destination node
+
+    Returns:
+        bool: whether a path between src and dest exists
+    """
+    ids_visited: List[int] = bfs_search(src)
+    return True if dest.id in ids_visited else False
+
+
+class TestRouteBetweenNodes:
+    def test_route_between_nodes(self):
+        n0 = Node(0, [])
+        n1 = Node(1, [])
+        n2 = Node(2, [])
+        n3 = Node(3, [])
+        n4 = Node(4, [])
+        n5 = Node(5, [])
+        n0.add_child(n1, n4, n5)
+        n1.add_child(n3, n4)
+        n3.add_child(n2, n4)
+        # There is a route from node 0 to node 2
+        self.assertTrue(route_between_nodes(n0, n2))
+        # No route between node 1 and node 0
+        self.assertFalse(route_between_nodes(n1, n0))
+        # There is a route from node 2 to node 3
+        self.assertTrue(route_between_nodes(n2, n3))
 
 class TestMyGraphSearch(unittest.TestCase):
 
