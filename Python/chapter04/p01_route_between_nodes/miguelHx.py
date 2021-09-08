39c084ce4086aee21900712cd5fd9e7574e0ddd0
miguelhx <mhernandez401405@gmail.com>
2021-09-07 20:30:54 -0700

    Remove code unused in solution

--- Python/chapter04/p01_route_between_nodes/miguelHx.py
+++ Python/chapter04/p01_route_between_nodes/miguelHx.py
@@ -42,31 +42,6 @@ class Node:
     def __str__(self):
         return f'Node ({self.id}), visited: {self.visited}'
 
-
-def dfs_search(root: Node) -> List[int]:
-    """Simple DFS.
-    takes in a root, returns a list
-    of ids of the sequence of visited
-    nodes.
-
-    Args:
-        root (Node): starting node
-
-    Returns:
-        List[int]: list of node IDs (i.e. [0, 1, 3])
-    """
-    output = []
-    if root is None:
-        return
-    # print(f'Visiting node ({root.id})')
-    root.visited = True
-    # print(root.children)
-    output.append(root.id)
-    for node in root.children:
-        if not node.visited:
-            output.extend(dfs_search(node))
-    return output
-
 def bfs_search(root: Node) -> List[int]:
     """Simple BFS.
     takes in a root, returns a list
@@ -161,20 +136,6 @@ class TestMyGraphSearch(unittest.TestCase):
         nodes = [n0, n1, n2, n3, n4, n5, n6]
         g = Graph(nodes)
         # g.print_graph()
-
-    def test_basic_depth_first_search(self):
-        n0 = Node(0, [])
-        n1 = Node(1, [])
-        n2 = Node(2, [])
-        n3 = Node(3, [])
-        n4 = Node(4, [])
-        n5 = Node(5, [])
-        n0.add_child(n1, n4, n5)
-        n1.add_child(n3, n4)
-        n2.add_child(n1)
-        n3.add_child(n2, n4)
-        result: List[int] = dfs_search(n0)
-        self.assertEqual(result, [0, 1, 3, 2, 4, 5])
 
     def test_basic_breadth_first_search(self):
         n0 = Node(0, [])
