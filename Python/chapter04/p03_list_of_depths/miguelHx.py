30af52fb9300bbd746b82f6d472b799d5de15655
miguelhx <mhernandez401405@gmail.com>
2021-09-23 11:14:58 -0700

    Setup basic test case, function signature, update BST data structure to be a regular Binary Tree rather than a Binary Search Tree

--- Python/chapter04/p03_list_of_depths/miguelHx.py
+++ Python/chapter04/p03_list_of_depths/miguelHx.py
@@ -7,9 +7,10 @@ a linked list of all the nodes at each depth
 import unittest
 
 from abc import abstractmethod
+from collections import deque
 from dataclasses import dataclass
 from typing import Generic, TypeVar
-from typing import Optional, Protocol
+from typing import Optional, Protocol, Deque
 from typing import Generator, List, Iterator
 
 
@@ -29,20 +30,20 @@ class Comparable(Protocol):
         pass
 
 @dataclass
-class BSTNode(Generic[T]):
+class BTNode(Generic[T]):
     val: T
-    left_child: 'Optional[BSTNode]' = None
-    right_child: 'Optional[BSTNode]' = None
+    left_child: 'Optional[BTNode]' = None
+    right_child: 'Optional[BTNode]' = None
 
     def __str__(self):
         return f'Node ({self.id}), Left ID: {self.left_child.id}, Right ID: {self.right_child.id}'
 
 class BSTIterator(Iterator[T]):
 
-    def __init__(self, root: Optional[BSTNode]):
+    def __init__(self, root: Optional[BTNode]):
         self.gen = self.in_order_traversal_generator(root)
 
-    def in_order_traversal_generator(self, node: Optional[BSTNode]) -> Generator:
+    def in_order_traversal_generator(self, node: Optional[BTNode]) -> Generator:
         if not node:
             raise StopIteration
         if node.left_child:
@@ -55,27 +56,27 @@ class BSTIterator(Iterator[T]):
         return next(self.gen)
 
 @dataclass
-class BinarySearchTree:
-    root: 'Optional[BSTNode]' = None
+class BinaryTree:
+    root: 'Optional[BTNode]' = None
 
     def insert(self, value: T) -> None:
         if not self.root:
-            self.root = BSTNode(value)
+            self.root = BTNode(value)
         else:
             self._insert(value, self.root)
 
-    def _insert(self, value: T, curr_node: BSTNode) -> None:
+    def _insert(self, value: T, curr_node: BTNode) -> None:
         if value < curr_node.val:
             if not curr_node.left_child:
                 # insert here
-                curr_node.left_child = BSTNode(value)
+                curr_node.left_child = BTNode(value)
             else:
                 # otherwise, keep searching left subtree
                 self._insert(value, curr_node.left_child)
         elif value > curr_node.val:
             if not curr_node.right_child:
                 # insert here
-                curr_node.right_child = BSTNode(value)
+                curr_node.right_child = BTNode(value)
             else:
                 # otherwise, keep searching right subtree
                 self._insert(value, curr_node.right_child)
@@ -85,7 +86,7 @@ class BinarySearchTree:
     def height(self) -> int:
         return self._height(self.root)
 
-    def _height(self, node: Optional[BSTNode]) -> int:
+    def _height(self, node: Optional[BTNode]) -> int:
         if not node:
             return 0
         else:
@@ -95,7 +96,7 @@ class BinarySearchTree:
         if self.root:
             self._print_tree(self.root)
 
-    def _print_tree(self, curr_node: Optional[BSTNode]) -> None:
+    def _print_tree(self, curr_node: Optional[BTNode]) -> None:
         if curr_node:
             self._print_tree(curr_node.left_child)
             print(curr_node.val)
@@ -105,29 +106,67 @@ class BinarySearchTree:
         return BSTIterator(self.root)
 
 
-class TestBinarySearchTree(unittest.TestCase):
+def list_of_depths(bt: BinaryTree) -> List[Deque[BTNode]]:
+    """Given a binary tree, design an algorithm which creates
+    a linked list of all the nodes at each depth
+    (e.g., if you have a tree with depth D, you'll have D linked lists).
+    Args:
+        bst (BinaryTree): input binary tree
+
+    Returns:
+        List[Deque[BTNode]]: list of nodes at each depth
+    """
+    return []
+
+
+
+class TestBinaryTree(unittest.TestCase):
 
     def test_binary_search_tree_creation_height_3(self):
-        bst = BinarySearchTree()
-        bst.insert(8)
-        bst.insert(4)
-        bst.insert(10)
-        bst.insert(2)
-        bst.insert(6)
-        bst.insert(20)
-        self.assertEqual(list(bst), [2, 4, 6, 8, 10, 20])
-        self.assertEqual(bst.height(), 3)
+        bt = BinaryTree()
+        bt.insert(8)
+        bt.insert(4)
+        bt.insert(10)
+        bt.insert(2)
+        bt.insert(6)
+        bt.insert(20)
+        self.assertEqual(list(bt), [2, 4, 6, 8, 10, 20])
+        self.assertEqual(bt.height(), 3)
 
     def test_binary_search_tree_creation_height_4(self):
-        bst = BinarySearchTree()
-        bst.insert(8)
-        bst.insert(2)
-        bst.insert(10)
-        bst.insert(4)
-        bst.insert(6)
-        bst.insert(20)
-        self.assertEqual(list(bst), [2, 4, 6, 8, 10, 20])
-        self.assertEqual(bst.height(), 4)
+        bt = BinaryTree()
+        bt.insert(8)
+        bt.insert(2)
+        bt.insert(10)
+        bt.insert(4)
+        bt.insert(6)
+        bt.insert(20)
+        self.assertEqual(list(bt), [2, 4, 6, 8, 10, 20])
+        self.assertEqual(bt.height(), 4)
+
+
+class TestListOfDepths(unittest.TestCase):
+
+    def test_list_of_depths_full_btree_height_3(self):
+        bt = BinaryTree()
+        bt.insert(8)
+        bt.insert(4)
+        bt.insert(10)
+        bt.insert(2)
+        bt.insert(6)
+        bt.insert(20)
+        self.assertEqual(list(bt), [2, 4, 6, 8, 10, 20])
+        self.assertEqual(bt.height(), 3)
+
+        expected_result = [
+            deque([8]),
+            deque([4, 10]),
+            deque([2, 6, 9, 10])
+        ]
+
+        result = list_of_depths(bt)
+
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
